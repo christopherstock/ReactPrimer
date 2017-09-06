ec700f19894179eda55600bee471f9ba8671011c
Christopher Stock <christopher.stock@ergodirekt.de>
2017-09-06 12:58:11 +0200

    Removed webpack externals. Merged everything into one bundle.js.

---
+++ bin/index.js
@@ -0,0 +1,17 @@
+/*
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+*/
+/*
+import { Hello } from "./components/Hello";
+*/
+window.onload = function () {
+    console.log("Test Fucker!");
+    /*
+        ReactDOM.render(
+            <Hello compiler="TypeScript" framework="React" />,
+            document.getElementById("example")
+        );
+    */
+};
+//# sourceMappingURL=index.js.map
